84275d1c2f62d41edbe1b60b5f6d9cc9b94700ab
romanusovich1 <romanusovich1@gmail.com>
2023-05-08 11:15:12 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const folderPath = path.join(__dirname, 'secret-folder')
+
+fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+        fs.stat(path.join(folderPath, file), (err, stats) => {
+            if (err) throw err;
+            const ext = path.extname(file);
+            const name = file.slice(0, -ext.length);
+            let size = stats.size;
+            const isFile = stats.isFile();
+            if (isFile) console.log(`${name} - ${ext.slice(1)} - ${size}b`);
+        });
+    });
+});
