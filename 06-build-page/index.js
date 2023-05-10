420b45d0a8958edba8d6ca47e0357dade9fe5da6
romanusovich1 <romanusovich1@gmail.com>
2023-05-10 15:53:31 +0300

    fix: edit 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,6 +7,7 @@ let stylesPath = path.join(__dirname, 'styles');
 let targetPath = path.join(__dirname, 'project-dist');
 
 fs.access(targetPath, (err) => {
+    // Create index.html file
     if (err) fs.mkdir(targetPath, (err) => { if (err) throw err; });
     fs.readFile(path.join(__dirname, 'template.html'), (err, index) => {
         if (err) throw err;
@@ -15,15 +16,17 @@ fs.access(targetPath, (err) => {
             if (err) throw err;
             files.forEach(file => {
                 let indexTemplate = file.slice(0, -path.extname(file).length);
-                fs.readFile(path.join(componentsPath, file), (err, component) => {
-                    if (err) throw err;
-                    indexString = indexString.replace(`{{${indexTemplate}}}`, component.toString());
-                    fs.writeFile(path.join(targetPath, 'index.html'), indexString, (err) => { if (err) throw err; });
+                let input = fs.createReadStream(path.join(componentsPath, file));
+                let output = fs.createWriteStream(path.join(targetPath, 'index.html'));
+                input.on('data', (data) => {
+                    indexString = indexString.replace(`{{${indexTemplate}}}`, data.toString());
+                    output.write(indexString);
                 });
             });
         });
     });
 
+    // Create style.css file
     fs.readdir(stylesPath, (err, files) => {
         if (err) throw err;
         fs.writeFile(
@@ -49,33 +52,28 @@ fs.access(targetPath, (err) => {
         });
     });
 
+    // Copy assets directory
     copyDir(assetsPath, path.join(targetPath, 'assets'));
     function copyDir(source, target) {
         fs.access(target, (err) => {
-            if (!err) fs.rm(target, { recursive: true }, (err) => {
+            if (err) fs.mkdir(target, { recursive: true }, (err) => { if (err) throw err; });
+            fs.readdir(source, (err, files) => {
                 if (err) throw err;
-                copy(source, target);
-                function copy(source, target) {
-                    fs.mkdir(target, (err) => {
+                files.forEach(file => {
+                    let sourceFile = path.join(source, file);
+                    let targetFile = path.join(target, file);
+                    fs.stat(sourceFile, (err, stats) => {
                         if (err) throw err;
+                        if (stats.isFile())
+                            fs.copyFile(
+                                sourceFile,
+                                targetFile,
+                                (err) => { 
+                                    if (err) throw err; 
+                                });
+                        else copyDir(sourceFile, targetFile);
                     });
-                    fs.readdir(source, (err, files) => {
-                        if (err) throw err;
-                        files.forEach(file => {
-                            let sourceFile = path.join(source, file);
-                            let targetFile = path.join(target, file);
-                            fs.stat(sourceFile, (err, stats) => {
-                                if (err) throw err;
-                                if (stats.isFile())
-                                    fs.copyFile(
-                                        sourceFile,
-                                        targetFile,
-                                        (err) => { if (err) throw err; });
-                                else copy(sourceFile, targetFile);
-                            });
-                        });
-                    });
-                }
+                });
             });
         });
     }
