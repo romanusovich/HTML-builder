e75679701965425a74369e5f8c883c4fd8f07f29
romanusovich1 <romanusovich1@gmail.com>
2023-05-10 11:21:53 +0300

    fix: edit 06-build-page copyDir func

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -52,25 +52,30 @@ fs.access(targetPath, (err) => {
     copyDir(assetsPath, path.join(targetPath, 'assets'));
     function copyDir(source, target) {
         fs.access(target, (err) => {
-            if (!err) fs.rm(target, { recursive: true, force: true }, (err) => {
+            if (!err) fs.rm(target, { recursive: true }, (err) => {
                 if (err) throw err;
-            });
-            fs.mkdir(target, { recursive: true }, (err) => { if (err) throw err; });
-            fs.readdir(source, (err, files) => {
-                if (err) throw err;
-                files.forEach(file => {
-                    let sourceFile = path.join(source, file);
-                    let targetFile = path.join(target, file);
-                    fs.stat(sourceFile, (err, stats) => {
+                copy(source, target);
+                function copy(source, target) {
+                    fs.mkdir(target, (err) => {
                         if (err) throw err;
-                        if (stats.isFile())
-                            fs.copyFile(
-                                sourceFile,
-                                targetFile,
-                                (err) => { if (err) throw err; });
-                        else copyDir(sourceFile, targetFile);
                     });
-                });
+                    fs.readdir(source, (err, files) => {
+                        if (err) throw err;
+                        files.forEach(file => {
+                            let sourceFile = path.join(source, file);
+                            let targetFile = path.join(target, file);
+                            fs.stat(sourceFile, (err, stats) => {
+                                if (err) throw err;
+                                if (stats.isFile())
+                                    fs.copyFile(
+                                        sourceFile,
+                                        targetFile,
+                                        (err) => { if (err) throw err; });
+                                else copy(sourceFile, targetFile);
+                            });
+                        });
+                    });
+                }
             });
         });
     }
